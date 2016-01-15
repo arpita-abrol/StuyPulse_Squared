662b88dfad0c242a9c6fc3d45b1af56ff7370b2b
Ling <lcheng3@stuy.edu>
2016-01-15 01:12:41 -0500

    Moved methods around and added catchPokemon()
    -File works
    -Deleted numPokeOnMe and numPokeInLab because they weren't necessary
    -Corrected the names of the potions and Pokeballs
    Added catch()- sends newly caught Pokemon to backpack or lab depending
    on the number of Pokemon in the backpack

--- Trainer.java
+++ Trainer.java
@@ -6,8 +6,8 @@ public class Trainer{
     
     private ArrayList<Pokemon> onMePokemon;//Array of the Pokemon on you
     private ArrayList<Pokemon> inLabPokemon;//Array of Pokemon stored in the lab
-    private ArrayList Pokeballs;//Array of your Pokeballs-Pokeball, Great Ball, Super Ball, Master Ball
-    private ArrayList Potions;//Array of your Potions- Potion, Super Potion,Ultra Potion,  Full Potion
+    private ArrayList Pokeballs;//Array of your Pokeballs-Pokeball, Great Ball, Ultra Ball, Master Ball
+    private ArrayList Potions;//Array of your Potions- Potion, Super Potion, Hyper Potion,  Full Potion
     private int Money;
     private int numPokeOnMe;
     private int numPokeInLab;
@@ -18,8 +18,6 @@ public class Trainer{
 	TrainerName= "Pokemon Trainer";
 	onMePokemon= new ArrayList<Pokemon>();//Trainer starts with no Pokemon
 	inLabPokemon= new ArrayList<Pokemon>();//Trainer starts with no Pokemon
-	numPokeOnMe= onMePokemon.size();
-	numPokeInLab= inLabPokemon.size();
 	createPokeballs();
 	createPotions();
 	Money= 500;//Trainer starts with 500 money
@@ -31,33 +29,6 @@ public class Trainer{
     }
     //================================================
 
-    //==============Methods==================================
-    //Trainers starts with 20 Pokeball, 10 Great Ball, 0 Super Ball, and 0 Master Ball
-    private void createPokeballs(){
-	Pokeballs= new ArrayList();
-	Pokeballs.add("Pokeballs: ");
-	Pokeballs.add(20);
-	Pokeballs.add("Great Balls: ");
-	Pokeballs.add(10);
-	Pokeballs.add("Super Balls: ");
-	Pokeballs.add(0);
-	Pokeballs.add("Master: ");
-	Pokeballs.add(0);
-    }
-    //Trainers starts with 20 Potions, 10 Super Potions, 5 Ultra Potions, and 0 Full Potions
-    private void createPotions(){
-	Potions= new ArrayList();
-	Potions.add("Potions: ");
-	Potions.add(20);
-	Potions.add("Super Potions: ");
-	Potions.add(10);
-	Potions.add("Ultra Potions: ");
-	Potions.add(5);
-	Potions.add("Full Potions: ");
-	Potions.add(0);
-    }
-    //================================================
-
     //=============Accessors===================================
     private String getName(){
 	return TrainerName;
@@ -66,13 +37,13 @@ public class Trainer{
 	return onMePokemon;
     }
     private int getNumPokeOnMe(){
-	return numPokeOnMe;
+	return onMePokemon.size();
     }
     private ArrayList getPokeInLab(){
 	return inLabPokemon;
     }
     private int getNumPokeInLab(){
-	return numPokeInLab;
+	return inLabPokemon.size();
     }
     private String getPotions(){
 	String retStr= "";
@@ -99,7 +70,7 @@ public class Trainer{
     /*Trainer chooses to buy:
     1: Potion
     2: Super Potion
-    3: Ultra Potion
+    3: Hyper Potion
     4: Full Potion
     name is the number they choose and num is the amount they bought*/
     private void setPotions(int name, int num){
@@ -107,10 +78,61 @@ public class Trainer{
 	name-=1;
 	Potions.set(name, ((Integer)(Potions.get(name)))+num);
     }
+    /*Trainer chooses to buy:
+    1: Pokeball
+    2: Great Ball
+    3: Ultra Ball
+    4: Master Ball
+    name is the number they choose and num is the amount they bought*/
+    private void setPokeballs(int name, int num){
+	name*=2;
+	name-=1;
+	Potions.set(name, ((Integer)(Potions.get(name)))+num);
+    }
+    private void setMoney(int newMoney){
+	Money= newMoney;
+    }
+    //================================================
+
+    //==============Methods==================================
+    //Trainers starts with 20 Pokeball, 10 Great Ball, 0 Ultra Ball, and 0 Master Ball
+    private void createPokeballs(){
+	Pokeballs= new ArrayList();
+	Pokeballs.add("Pokeballs: ");
+	Pokeballs.add(20);
+	Pokeballs.add("Great Balls: ");
+	Pokeballs.add(10);
+	Pokeballs.add("Ultra Balls: ");
+	Pokeballs.add(0);
+	Pokeballs.add("Master: ");
+	Pokeballs.add(0);
+    }
+    //Trainers starts with 20 Potions, 10 Super Potions, 5 Hyper Potions, and 0 Full Potions
+    private void createPotions(){
+	Potions= new ArrayList();
+	Potions.add("Potions: ");
+	Potions.add(20);
+	Potions.add("Super Potions: ");
+	Potions.add(10);
+	Potions.add("Hyper Potions: ");
+	Potions.add(5);
+	Potions.add("Full Potions: ");
+	Potions.add(0);
+    }
+    //Sends newly caught Pokemon to your backpack or lab depending on how many Pokemon you're carrying
+    private void catchPokemon(Pokemon newPokemon){
+	if (getNumPokeOnMe()> 5){
+	    inLabPokemon.add(newPokemon);
+	    System.out.println("Max number of Pokemon you can carry was reached, so Pokemon caught was sent to lab.");
+	}else{
+	    onMePokemon.add(newPokemon);
+	    System.out.println("Pokemon caught was sent to your backpack. You now have "+getNumPokeOnMe()+" Pokemon on you.");
+	}
+	
+    }
+    //================================================
 
     
-
-    //================================================
     public static void main(String[] args){
 	Trainer test= new Trainer("Ling");
 	System.out.println("Your name is Trainer "+test.getName());
@@ -124,4 +146,4 @@ public class Trainer{
 	test.setPotions(1, 10);
 	System.out.println(test.getPotions());
     }
-}
+} 
