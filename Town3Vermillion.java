c69302c97efa29a849ce325b5be428d5c2807051
Arpita <arpita-abrol@users.noreply.github.com>
2016-01-26 07:30:00 -0500

    made into boolean
    made into boolean

--- Town3Vermillion.java
+++ Town3Vermillion.java
@@ -3,7 +3,7 @@ import cs1.Keyboard;
 
 public class Town3Vermillion {
 
-    public void gym( Trainer player ) {
+    public boolean gym( Trainer player ) {
 
 	System.out.println("Welcome to the Vermillion City Gym. The Gym Leader is Misty. \nTo prove that you are worthy of challenging the gym leader, Lt. Surge, you must first beat some fellow Pokemon Trainers. Good luck, Trainer.");
 	
@@ -12,39 +12,39 @@ public class Town3Vermillion {
 	player.battlePokemonT(SS1);
 	if(!player.stillAlive() ) {
 	    System.out.println("Electrician Ernie says: My Pokemon are my best friends!");
-	    return;
+	    return false;
 	}
 	Pokemon SS2 = new Pokemon("Electabuzz",18);
 	player.battlePokemon(SS2);
 	if(!player.stillAlive() ) {
 	    System.out.println("Electrician Ernie says: My Pokemon are my best friends!");
-	    return;
+	    return false;
 	}
 	player.setMoney( player.getMoney() + 450 );
 	System.out.println("You have defeated Electrician Ernie. You win 450 PokeDollars! \nElectrician Ernie says: My precious pokemon...");
 
 	
-	System.out.println(" ! \nSailor Butch challenges you to a battle!");
+	System.out.println(" ! \nElectrician Ernie challenges you to a battle!");
 	Pokemon SB1 = new Pokemon("Magnemite",10);
 	player.battlePokemonT(SB1);
 	if(!player.stillAlive() ) {
 	    System.out.println("Student Sally says: Water Pokemon are the best!");
-	    return;
+	    return false;
 	}
 	Pokemon SB2 = new Pokemon("Magnemite",13);
 	player.battlePokemonT(SB2);
 	if(!player.stillAlive() ) {
 	    System.out.println("Student Sally says: Water Pokemon are the best!");
-	    return;
+	    return false;
 	}
 	Pokemon SB3 = new Pokemon("Magnemite",15);
 	player.battlePokemonT(SB3);
 	if(!player.stillAlive() ) {
 	    System.out.println("Student Sally says: Water Pokemon are the best!");
-	    return;
+	    return false;
 	}
 	player.setMoney( player.getMoney() + 500 );
-	System.out.println("You have defeated Student Sally. You win 500 PokeDollars! \nMiner Will says: H-how could this be... ");
+	System.out.println("You have defeated Student Sally. You win 500 PokeDollars! \nStudent Sally says: H-how could this be... ");
 
 	
 	System.out.println("You have defeated all other trainers. \nYou have challenged Gym Leader Misty!");
@@ -52,23 +52,24 @@ public class Town3Vermillion {
 	player.battlePokemonT(GLM1);
 	if(!player.stillAlive() ) {
 	    System.out.println("Lt. Surge says: Better luck next time, kid");
-	    return;
+	    return false;
 	}
 	Pokemon GLM2 = new Pokemon("Pikachu",20);
 	player.battlePokemonT(GLM2);
 	if(!player.stillAlive() ) {
 	    System.out.println("Lt. Surge says: Better luck next time, kid");
-	    return;
+	    return false;
 	}
 		Pokemon GLM3 = new Pokemon("Raichu",28);
 	player.battlePokemonT(GLM3);
 	if(!player.stillAlive() ) {
 	    System.out.println("Lt. Surge says: Better luck next time, kid");
-	    return;
+	    return false;
 	}
 	player.setMoney( player.getMoney() + 1000 );
 	System.out.println("Good job, kid. Take this Thunder Badge; you deserve it.");
-	
+
+	return true;
     }
     
 } //end class
