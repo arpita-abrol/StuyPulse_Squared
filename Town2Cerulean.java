5b72214fbc5d4182917985f44cd9bacb44c769b0
Arpita <arpita-abrol@users.noreply.github.com>
2016-01-26 07:28:06 -0500

    make into boolean
    made into boolean

--- Town2Cerulean.java
+++ Town2Cerulean.java
@@ -3,7 +3,7 @@ import cs1.Keyboard;
 
 public class Town2Cerulean {
 
-    public void gym( Trainer player ) {
+    public boolean gym( Trainer player ) {
 
 	System.out.println("Welcome to the Cerulean City Gym. The Gym Leader is Misty. \nTo prove that you are worthy of challenging Gym Leader Misty, you must first beat some fellow Pokemon Trainers. Good luck, Trainer.");
 	
@@ -12,16 +12,16 @@ public class Town2Cerulean {
 	player.battlePokemonT(SS1);
 	if(!player.stillAlive() ) {
 	    System.out.println("Sailor Sabrina says: My Pokemon are my best friends!");
-	    return;
+	    return false;
 	}
 	Pokemon SS2 = new Pokemon("Tentacool",18);
 	player.battlePokemon(SS2);
 	if(!player.stillAlive() ) {
 	    System.out.println("Sailor Sabrina says: My Pokemon are my best friends!");
-	    return;
+	    return false;
 	}
 	player.setMoney( player.getMoney() + 250 );
-	System.out.println("You have defeated PokeFan Cassidy. You win 250 PokeDollars! \nPokeFan Cassidy says: NOOO, I must not be a big enough fan :(");
+	System.out.println("You have defeated Sailor Sabrina. You win 250 PokeDollars! \nSailor Sabrina says: NOOO, I must not be a big enough fan :(");
 
 	
 	System.out.println(" ! \nSailor Butch challenges you to a battle!");
@@ -29,22 +29,22 @@ public class Town2Cerulean {
 	player.battlePokemonT(SB1);
 	if(!player.stillAlive() ) {
 	    System.out.println("Sailor Butch says: Water Pokemon are the best!");
-	    return;
+	    return false;
 	}
 	Pokemon SB2 = new Pokemon("Tentacool",15);
 	player.battlePokemonT(SB2);
 	if(!player.stillAlive() ) {
 	    System.out.println("Sailor Butch says: Water Pokemon are the best!");
-	    return;
+	    return false;
 	}
 	Pokemon SB3 = new Pokemon("Tentacool",15);
 	player.battlePokemonT(SB3);
 	if(!player.stillAlive() ) {
 	    System.out.println("Sailor Butch says: Water Pokemon are the best!");
-	    return;
+	    return false;
 	}
 	player.setMoney( player.getMoney() + 500 );
-	System.out.println("You have defeated Miner Will. You win 500 PokeDollars! \nMiner Will says: H-how could this be... ");
+	System.out.println("You have defeated Sailor Butch. You win 500 PokeDollars! \nSailor Butch says: H-how could this be... ");
 
 	
 	System.out.println("You have defeated all other trainers. \nYou have challenged Gym Leader Misty!");
@@ -52,23 +52,23 @@ public class Town2Cerulean {
 	player.battlePokemonT(GLM1);
 	if(!player.stillAlive() ) {
 	    System.out.println("Gym Leader Misty says: Better luck next time, kid");
-	    return;
+	    return false;
 	}
 	Pokemon GLM2 = new Pokemon("Starmie",25);
 	player.battlePokemonT(GLM2);
 	if(!player.stillAlive() ) {
 	    System.out.println("Gym Leader Misty says: Better luck next time, kid");
-	    return;
+	    return false;
 	}
 		Pokemon GLM3 = new Pokemon("Psyduck",23);
 	player.battlePokemonT(GLM3);
 	if(!player.stillAlive() ) {
 	    System.out.println("Gym Leader Misty says: Better luck next time, kid");
-	    return;
+	    return false;
 	}
 	player.setMoney( player.getMoney() + 1000 );
 	System.out.println("Good job, kid. Take this Cascade Badge; you deserve it.");
-	
+	return true;
     }
     
 } //end class
